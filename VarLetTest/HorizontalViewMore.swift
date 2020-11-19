e1a2a2e9cb1b24e51913c2073df5b3216146b83c
Kirty Kumari <kirty.kumari@ymail.com>
2020-11-19 16:03:00 +0530

    Initial commit

---
+++ VarLetTest/HorizontalViewMore.swift
@@ -0,0 +1,56 @@
+
+import UIKit
+
+class HorizontalViewMore: UIView {
+    private weak var lblContainer : UIView!
+    private weak var titleLbl : UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initializeUI(){
+        createViews()
+        setFonts()
+    }
+    
+    func setFonts() {
+        self.titleLbl.font = UIFont.systemFont(ofSize: 14.0)
+        self.titleLbl.textColor = UIColor.green
+        self.titleLbl.textAlignment = .center
+        self.titleLbl.numberOfLines = 0
+        self.titleLbl.text = "VIEW MORE"
+    }
+    
+    func createViews() {
+        var view : UIView = UIView.init(frame: CGRect.zero)
+        self.lblContainer = view;
+        //self.lblContainer.isUserInteractionEnabled = false
+        self.lblContainer.translatesAutoresizingMaskIntoConstraints = false;
+        self.addSubview(self.lblContainer)
+        self.lblContainer.backgroundColor = UIColor.clear;
+        self.lblContainer.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true;
+        self.lblContainer.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true;
+        self.lblContainer.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor, constant: 8.0).isActive = true
+        self.lblContainer.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor, constant: -8.0).isActive = true
+        
+        let lbl = UILabel.init(frame: CGRect.zero);
+        self.titleLbl = lbl;
+        self.titleLbl.isUserInteractionEnabled = false
+        self.titleLbl.numberOfLines = 2;
+        self.titleLbl.translatesAutoresizingMaskIntoConstraints = false;
+        self.lblContainer.addSubview(self.titleLbl)
+        self.titleLbl.leadingAnchor.constraint(equalTo: self.lblContainer.leadingAnchor).isActive = true
+        self.titleLbl.trailingAnchor.constraint(equalTo: self.lblContainer.trailingAnchor).isActive = true
+        self.titleLbl.topAnchor.constraint(equalTo: self.lblContainer.topAnchor).isActive = true
+    }
+    
+    deinit {
+        print("MemTest : HorizontalViewMore is deinitialized")
+    }
+}
---
+++ VarLetTest/ViewController.swift
@@ -0,0 +1,28 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private weak var horizontalViewMore : HorizontalViewMore!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let footerView = HorizontalViewMore()
+        self.horizontalViewMore = footerView
+        self.horizontalViewMore.initializeUI()
+        self.horizontalViewMore.isUserInteractionEnabled = false
+        self.horizontalViewMore.translatesAutoresizingMaskIntoConstraints = false;
+        self.horizontalViewMore.layer.borderWidth = 1.0;
+        self.horizontalViewMore.layer.borderColor = UIColor.red.cgColor
+        self.horizontalViewMore.backgroundColor = .white
+        self.view.addSubview(self.horizontalViewMore)
+        self.horizontalViewMore.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.horizontalViewMore.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.horizontalViewMore.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.horizontalViewMore.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+
+
+}
+
